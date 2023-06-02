a419defbd7887506271b5fba1751ffc080c697ff
kcenteno <kcentenog@outlook.com>
2023-06-02 14:18:28 -0600

    xs

--- M03 JavaScript Fundamentos/Ejercicio 05.js
+++ M03 JavaScript Fundamentos/Ejercicio 05.js
@@ -8,6 +8,11 @@ function esPositivo(num) {
    // Si el número es negativo ---> "Es negativo".
    // Si el número es 0, devuelve false.
    // Tu código:
+   if (num >= 0) {
+      return("Es positivo");
+   } else {
+      return("Es negativo")
+   }
 }
 
 function agregarSimboloExclamacion(str) {
