a1710d5390347a7584c3c1fd2d45b4c8868a80d6
kcenteno <kcentenog@outlook.com>
2023-06-03 15:46:10 -0600

    Modulo 3 ejercicio 5

--- M03 JavaScript Fundamentos/Ejercicio 05.js
+++ M03 JavaScript Fundamentos/Ejercicio 05.js
@@ -8,10 +8,13 @@ function esPositivo(num) {
    // Si el número es negativo ---> "Es negativo".
    // Si el número es 0, devuelve false.
    // Tu código:
-   if (num >= 0) {
+   if (num > 0) {
       return("Es positivo");
-   } else {
-      return("Es negativo")
+   } else if (num < 0){
+      return("Es negativo");
+   }
+   else{
+      return(false);
    }
 }
 
@@ -33,29 +36,33 @@ function obtenerSaludo(nombre) {
    // Toma el string "nombre" y concatena otra string en la cadena para que tome la siguiente forma:
    // Ejemplo: "Martin" ---> "Hola Martin!"
    // Tu código:
-   return("Hola " + nombre);
+   return("Hola " + nombre + "!");
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
    // Retornar el área de un rectángulo teniendo su altura y ancho.
    // Tu código:
+   return(alto * ancho);
 }
 
 function retornarPerimetro(lado) {
    // La función recibe como argumento la medida de un lado de un cuadrado.
    // Debes retornar su perímetro.
    // Tu código:
+   return(lado * 4);
 }
 
 function areaDelTriangulo(base, altura) {
    // Calcula el área de un triángulo y retorna el resultado.
    // Tu código:
+   return((base * altura) / 2)
 }
 
 function deEuroAdolar(euro) {
    // Supongamos que 1 euro equivale a 1.20 dólares.
    // Debes calcular el valor recibido como argumento pasándolo a dolares.
    // Tu código:
+   return(euro * 1.20)
 }
 
 function esVocal(letra) {
@@ -63,6 +70,26 @@ function esVocal(letra) {
    // Si el usuario ingresa un string de más de un caracter debes retornar el mensaje: "Dato incorrecto".
    // Si no es vocal, tambien debe retornar "Dato incorrecto".
    // Tu código:
+   switch (letra) {
+      case "a":
+         return("Es vocal");
+         break;
+      case "e":
+         return("Es vocal");
+         break;
+      case "i":
+         return("Es vocal");
+         break;
+      case "o":
+         return("Es vocal");
+         break;
+      case "u":
+         return("Es vocal");
+         break;
+      default:
+         return("Dato incorrecto")
+         break;
+   }
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
